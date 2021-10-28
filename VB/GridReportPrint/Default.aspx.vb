64fedbe5015799f0adde81a1ccdfa80654959538
DevExpressExampleBot <fakeEmail>
2021-10-28 17:13:33 +0300

    autogenerate VB

--- VB/GridReportPrint/Default.aspx.vb
+++ VB/GridReportPrint/Default.aspx.vb
@@ -1,9 +1,4 @@
-﻿Imports System
-Imports System.Data
-Imports System.Configuration
-Imports System.Collections
-Imports System.Web
-Imports System.Web.Security
+Imports System
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports System.Web.UI.WebControls.WebParts
@@ -14,26 +9,25 @@ Imports DevExpress.Utils.Design
 Imports DevExpress.XtraPrinting
 
 Namespace GridReportPrint
-	Partial Public Class _Default
-		Inherits System.Web.UI.Page
 
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-			If Me.Request.QueryString("isPrint") = "true" Then
-				Dim categoryID As Integer = Convert.ToInt32(Me.Request.QueryString("categoryID"))
-				Using report As XtraReport = New ProductsReport()
-				Using ms As New MemoryStream()
-					report.Parameters("CategoryID").Visible = False
-					report.Parameters("CategoryID").Value = categoryID
+    Public Partial Class _Default
+        Inherits Page
 
-					report.ExportToPdf(ms, New PdfExportOptions() With {.ShowPrintDialogOnOpen = True})
-
-					Me.Response.ContentType = "application/pdf"
-					Me.Response.Clear()
-					Me.Response.BinaryWrite(ms.ToArray())
-					Me.Response.End()
-				End Using
-				End Using
-			End If
-		End Sub
-	End Class
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            If Equals(Request.QueryString("isPrint"), "true") Then
+                Dim categoryID As Integer = Convert.ToInt32(Request.QueryString("categoryID"))
+                Using report As XtraReport = New ProductsReport()
+                    Using ms As MemoryStream = New MemoryStream()
+                        report.Parameters("CategoryID").Visible = False
+                        report.Parameters("CategoryID").Value = categoryID
+                        report.ExportToPdf(ms, New PdfExportOptions() With {.ShowPrintDialogOnOpen = True})
+                        Response.ContentType = "application/pdf"
+                        Response.Clear()
+                        Response.BinaryWrite(ms.ToArray())
+                        Response.End()
+                    End Using
+                End Using
+            End If
+        End Sub
+    End Class
 End Namespace
